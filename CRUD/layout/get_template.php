7cbb58b832a03a0ce3a2e454eec15ac0d524fdad
davudmuradli <133804991+davudmuradli@users.noreply.github.com>
2023-07-23 23:57:22 +0400

    Delete CRUD directory

--- CRUD/layout/get_template.php
+++
@@ -1,6 +0,0 @@
-<?php 
-function get_template($file) {
-  include ('head.php');
-  include ('pages/' . $file . '/index.php');
-  include ('footer.php');  
-}
--- CRUD/layout/menu.php
+++
@@ -1,7 +0,0 @@
-<header data-bs-theme="dark">
-    <ul>
-      <li> <a href="./index.php?page=login">Login</a> </li>
-      <li> <a href="./index.php?page=product">Product</a> </li>
-      <li> <a href="./index.php?page=users">Users</a> </li>
-    </ul>
-</header>
